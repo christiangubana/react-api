6b8478dc32061f5a05c8311dd76f47c40f9310b7
christiangubana <christiangubana@gmail.com>
2023-01-23 10:45:53 +0200

    consuming api-data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,51 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  h1 {
+  h4 {
     color: #fab005;
+    text-transform: uppercase;
+    padding: 10px
   }
 `;
-
 const Home = () => {
   const [loading, setIsLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
   const [items, setItems] = useState([]);
 
-  console.log(items.results)
-
-useEffect(() => {
-  setIsLoading(true)
-  try {
-    fetch('http://dev3.elemental.co.za/elemental-cms/front_end/get_knowledge')
-    .then(response => response.json())
-    .then(data => setItems(data))
-    setIsLoading(false)
-  } catch (e) {
-    setHasError(e);
-    console.error(e)
-  }
-}, [])
-
+  useEffect(() => {
+    axios
+      .get("http://dev3.elemental.co.za/elemental-cms/front_end/get_knowledge")
+      .then((response) => {
+        console.log("RESULT", response.data.results);
+        setItems(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    <Wrapper>
-      <h1>Home</h1>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <h4>Category accordion items</h4>
+      </Wrapper>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          items?.map((item, index) => {
+            console.log(typeof item)
+            return (
+              <div key={index}>
+                <p>{item.cat}</p>
+              </div>
+            );
+          })
+        )}
+      </div>
+    </>
   );
 };
 
